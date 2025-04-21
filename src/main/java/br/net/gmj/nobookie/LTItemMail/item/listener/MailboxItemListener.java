275c96eb53ebae014b110c847469f6d581182763
BuildTools <31146142+leothawne@users.noreply.github.com>
2025-04-21 18:49:37 -0300

    Preventing mailboxes from being washed on a cauldron

--- src/main/java/br/net/gmj/nobookie/LTItemMail/item/listener/MailboxItemListener.java
+++ src/main/java/br/net/gmj/nobookie/LTItemMail/item/listener/MailboxItemListener.java
@@ -1,9 +1,12 @@
 package br.net.gmj.nobookie.LTItemMail.item.listener;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.event.block.CauldronLevelChangeEvent;
 import org.bukkit.inventory.ItemStack;
 
 import br.net.gmj.nobookie.LTItemMail.util.BukkitUtil;
@@ -13,5 +16,17 @@ public final class MailboxItemListener implements Listener {
 	public final void onBlockDispense(final BlockDispenseEvent e) {
 		final ItemStack mailbox = e.getItem();
 		if(mailbox != null && BukkitUtil.DataContainer.Mailbox.isMailbox(mailbox)) e.setCancelled(true);
+	}
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public final void onShulkerWash(final CauldronLevelChangeEvent e) {
+		Player player = null;
+		if(e.getEntity() != null && e.getEntity() instanceof Player) player = (Player) e.getEntity();
+		if(player != null && e.getReason().equals(CauldronLevelChangeEvent.ChangeReason.SHULKER_WASH)) {
+			ItemStack hand = null;
+			if(!player.getInventory().getItemInMainHand().getType().equals(Material.AIR)) {
+				hand = player.getInventory().getItemInMainHand();
+			} else if(!player.getInventory().getItemInOffHand().getType().equals(Material.AIR)) hand = player.getInventory().getItemInOffHand();
+			if(hand != null && BukkitUtil.DataContainer.Mailbox.isMailbox(hand)) e.setCancelled(true);
+		}
 	}
 }
