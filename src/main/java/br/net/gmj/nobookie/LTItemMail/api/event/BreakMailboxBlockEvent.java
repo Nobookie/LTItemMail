eecb666e84121ec6415354b32b2c6bf845e9350b
BuildTools <31146142+leothawne@users.noreply.github.com>
2025-09-22 18:36:55 -0300

    Update BreakMailboxBlockEvent.java

--- src/main/java/br/net/gmj/nobookie/LTItemMail/api/event/BreakMailboxBlockEvent.java
+++ src/main/java/br/net/gmj/nobookie/LTItemMail/api/event/BreakMailboxBlockEvent.java
@@ -1,9 +1,11 @@
 package br.net.gmj.nobookie.LTItemMail.api.event;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.NotNull;
 
 import br.net.gmj.nobookie.LTItemMail.api.block.MailboxBlock;
 /**
@@ -16,31 +18,48 @@ import br.net.gmj.nobookie.LTItemMail.api.block.MailboxBlock;
 public class BreakMailboxBlockEvent extends Event {
 	private static final HandlerList handlers = new HandlerList();
 	private final MailboxBlock block;
-	private Reason reason;
+	private final Reason reason;
 	private final Boolean virtual;
 	private final ClaimProvider provider;
-	public BreakMailboxBlockEvent(final MailboxBlock block, final Reason reason, final Boolean virtual, final ClaimProvider provider) {
+	private final Event providerEvent;
+	/**
+	 * 
+	 * The constructor of the event.
+	 * 
+	 * @param block The {@link MailboxBlock} object
+	 * @param reason The reason of the event
+	 * @param virtual Indicates whether the event was purely virtual or not
+	 * @param provider The claim provider if there is any
+	 * @param providerEvent The original event from Bukkit
+	 * 
+	 */
+	public BreakMailboxBlockEvent(final MailboxBlock block, final Reason reason, final Boolean virtual, final ClaimProvider provider, final Event providerEvent) {
 		super(!Bukkit.getServer().isPrimaryThread());
 		this.block = block;
 		this.reason = reason;
 		this.virtual = virtual;
 		this.provider = provider;
+		this.providerEvent = providerEvent;
 	}
 	/**
 	 * 
 	 * Gets the block affected by this event.
 	 * 
+	 * @return {@link MailboxBlock}
+	 * 
 	 */
-	@NotNull
+	@Nonnull
 	public final MailboxBlock getBlock() {
 		return block;
 	}
 	/**
 	 * 
-	 * "true" means that the block was removed from the database and it remains on the game as a vanilla shulker box.
+	 * Indicates whether the event was purely virtual or not
+	 * 
+	 * @return {@link Boolean#TRUE} if the block was removed from the database but remains placed on the current world as a vanilla Shulker Box.
 	 * 
 	 */
-	@NotNull
+	@Nonnull
 	public final Boolean isVirtual() {
 		return virtual;
 	}
@@ -48,7 +67,14 @@ public class BreakMailboxBlockEvent extends Event {
 	public final HandlerList getHandlers() {
 		return handlers;
 	}
-	@NotNull
+	/**
+	 * 
+	 * See {@link HandlerList}
+	 * 
+	 * @return {@link HandlerList}
+	 * 
+	 */
+	@Nonnull
 	public static final HandlerList getHandlerList() {
 		return handlers;
 	}
@@ -56,8 +82,10 @@ public class BreakMailboxBlockEvent extends Event {
 	 * 
 	 * Gets what triggered the event.
 	 * 
+	 * @return {@link Reason}
+	 * 
 	 */
-	@NotNull
+	@Nonnull
 	public final Reason getReason() {
 		return reason;
 	}
@@ -65,9 +93,23 @@ public class BreakMailboxBlockEvent extends Event {
 	 * 
 	 * Gets what plugin was involved on the break event if the reason is {@link Reason#ON_UNCLAIM}. Otherwise it will return null.
 	 * 
+	 * @return {@link ClaimProvider} or null
+	 * 
 	 */
+	@Nullable
 	public final ClaimProvider getClaimProvider() {
 		return provider;
+	}
+	/**
+	 * 
+	 * Gets the original event from Bukkit.
+	 * 
+	 * @return {@link Event} or null
+	 * 
+	 */
+	@Nullable
+	public final Event getClaimProviderEvent() {
+		return providerEvent;
 	}
 	/**
 	 * 
@@ -77,9 +119,29 @@ public class BreakMailboxBlockEvent extends Event {
 	 * 
 	 */
 	public enum Reason {
+		/**
+		 * 
+		 * Called on unclaim.
+		 * 
+		 */
 		ON_UNCLAIM,
+		/**
+		 * 
+		 * Called by owner.
+		 * 
+		 */
 		BY_PLAYER_OWNER,
+		/**
+		 * 
+		 * Called by admin.
+		 * 
+		 */
 		BY_PLAYER_ADMIN,
+		/**
+		 * 
+		 * Called by server.
+		 * 
+		 */
 		BY_SERVER
 	}
 	/**
@@ -90,8 +152,29 @@ public class BreakMailboxBlockEvent extends Event {
 	 * 
 	 */
 	public enum ClaimProvider {
+		/**
+		 * 
+		 * Called by GriefPrevention.
+		 * 
+		 */
 		GRIEFPREVENTION,
+		/**
+		 * 
+		 * Called by RedProtect.
+		 * 
+		 */
 		REDPROTECT,
-		TOWNYADVANCED
+		/**
+		 * 
+		 * Called by TownyAdvanced.
+		 * 
+		 */
+		TOWNYADVANCED,
+		/**
+		 * 
+		 * Called by Lands.
+		 * 
+		 */
+		LANDS
 	}
 }
