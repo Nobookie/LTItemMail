65060c8fa1fdc59e7832af7c9c799597d1e908e8
BuildTools <31146142+leothawne@users.noreply.github.com>
2025-09-22 18:37:05 -0300

    Update PlayerBreakMailboxBlockEvent.java

--- src/main/java/br/net/gmj/nobookie/LTItemMail/api/event/PlayerBreakMailboxBlockEvent.java
+++ src/main/java/br/net/gmj/nobookie/LTItemMail/api/event/PlayerBreakMailboxBlockEvent.java
@@ -2,8 +2,9 @@ package br.net.gmj.nobookie.LTItemMail.api.event;
 
 import java.util.Objects;
 
+import javax.annotation.Nonnull;
+
 import org.bukkit.event.Cancellable;
-import org.jetbrains.annotations.NotNull;
 
 import br.net.gmj.nobookie.LTItemMail.api.block.MailboxBlock;
 import br.net.gmj.nobookie.LTItemMail.api.entity.LTPlayer;
@@ -17,35 +18,36 @@ import br.net.gmj.nobookie.LTItemMail.api.entity.LTPlayer;
 public class PlayerBreakMailboxBlockEvent extends BreakMailboxBlockEvent implements Cancellable {
 	private final LTPlayer player;
 	private Boolean cancelled = false;
+	/**
+	 * 
+	 * The constructor of the event.
+	 * 
+	 * @param block The {@link MailboxBlock} object
+	 * @param reason The reason of the event
+	 * @param player The player involved on the event
+	 * 
+	 */
 	public PlayerBreakMailboxBlockEvent(final MailboxBlock block, final BreakMailboxBlockEvent.Reason reason, final LTPlayer player) {
-		super(block, reason, false, null);
+		super(block, reason, false, null, null);
 		this.player = player;
 	}
 	/**
 	 * 
 	 * Gets who is involved on this event.
 	 * 
+	 * @return {@link LTPlayer} object representing a player.
+	 * 
 	 */
-	@NotNull
+	@Nonnull
 	public final LTPlayer getPlayer() {
 		return player;
 	}
-	/**
-	 * 
-	 * Gets if the event was cancelled.
-	 * 
-	 */
 	@Override
 	public final boolean isCancelled() {
 		return cancelled;
 	}
-	/**
-	 * 
-	 * Cancels the event. If cancelled, the block will not be broken.
-	 * 
-	 */
 	@Override
-	public final void setCancelled(@NotNull final boolean cancel) throws NullPointerException {
+	public final void setCancelled(@Nonnull final boolean cancel) throws NullPointerException {
 		Objects.requireNonNull(cancel);
 		cancelled = cancel;
 	}
